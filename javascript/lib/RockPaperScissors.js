3d76b04717969cbd6de51772a91f0d8d16ead815
Tr1ckX <attila.mirk@gmail.com>
2014-11-25 16:40:07 +0000

    random choice for computer

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -6,23 +6,31 @@ Player.prototype.picks = function(pick) {
   this.pick = pick;
 };
 
+Game.prototype.random_pick = function () {
+  var picks, computer_choice;
+  picks = (Object.keys(this.defeat));
+  computer_choice = picks[Math.floor(Math.random() * picks.length)];
+};
 
 function Game(player1, player2) {
   this.player1 = player1;
   this.player2 = player2;
 };
 
+Game.prototype.defeat = {
+ 'rock': ['scissors', 'lizard'],
+ 'paper': ['rock', 'spock'],
+ 'scissors': ['paper', 'lizard'],
+ 'lizard': ['paper', 'spock'],
+ 'spock': ['scissors', 'rock']
+};
+
 Game.prototype.winner = function() {
 
-  var defeat = { 'rock': ['scissors', 'lizard'],
-                 'paper': ['rock', 'spock'],
-                 'scissors': ['paper', 'lizard'],
-                 'lizard': ['paper', 'spock'],
-                 'spock': ['scissors', 'rock'] }
 
   if (this.player1.pick == this.player2.pick) return null
 
-    array = defeat[this.player1.pick]
+    array = this.defeat[this.player1.pick]
 
     for (var i in array) {
 
