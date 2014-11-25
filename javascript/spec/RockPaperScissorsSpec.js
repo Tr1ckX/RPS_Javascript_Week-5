9c952d800a981bdb382a34f1090d03deb550cfe6
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-11-25 15:31:06 +0000

    added more tests for spock and lizard

--- javascript/spec/RockPaperScissorsSpec.js
+++ javascript/spec/RockPaperScissorsSpec.js
@@ -21,10 +21,26 @@ describe("Rock-Paper-Scissors", function() {
 
       });
 
+       it('should beat lizard', function() {
+
+        player1.picks('rock');
+        player2.picks('lizard');
+        expect(game.winner()).toBe(player1);
+
+      });
+
       it('should lose to paper', function() {
 
         player1.picks('rock');
         player2.picks('paper');
+        expect(game.winner()).toBe(player2);
+
+      });
+
+      it('should lose to spock', function() {
+
+        player1.picks('rock');
+        player2.picks('spock');
         expect(game.winner()).toBe(player2);
 
       });
@@ -41,10 +57,26 @@ describe("Rock-Paper-Scissors", function() {
 
       });
 
+      it('should beat spock', function() {
+
+        player2.picks('paper');
+        player1.picks('spock');
+        expect(game.winner()).toBe(player2);
+
+      });
+
       it('should lose to scissors', function() {
 
         player1.picks('paper');
         player2.picks('scissors');
+        expect(game.winner()).toBe(player2);
+
+      });
+
+      it('should lose to lizard', function() {
+
+        player1.picks('paper');
+        player2.picks('lizard');
         expect(game.winner()).toBe(player2);
 
       });
@@ -61,10 +93,26 @@ describe("Rock-Paper-Scissors", function() {
 
       });
 
+        it('should beat lizard', function() {
+
+        player1.picks('scissors');
+        player2.picks('lizard');
+        expect(game.winner()).toBe(player1);
+
+      });
+
       it('should lose to rock', function() {
 
         player1.picks('scissors');
         player2.picks('rock');
+        expect(game.winner()).toBe(player2);
+
+      });
+
+      it('should lose to spock', function() {
+
+        player1.picks('scissors');
+        player2.picks('spock');
         expect(game.winner()).toBe(player2);
 
       });
